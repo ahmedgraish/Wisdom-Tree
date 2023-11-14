85b0e1c5d88f7c82445d182eb9e498ec633e5b84
Ahmed graish <graish333@gmail.com>
2023-11-14 17:17:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            navigation:['Apply','dsa','dad','dd','dd']
+        }
+    },
+    methods: {
+        
+    },
+}).mount('#app')
