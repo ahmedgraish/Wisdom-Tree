705eda9619be9217e8a60f9a6f5dd30f4a1cfd65
Ahmed graish <graish333@gmail.com>
2023-11-15 13:47:32 +0200

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 const app = Vue.createApp({
     data() {
         return {
-            navigation:['Apply','dsa','dad','dd','dd']
+            videoPlayPack:['fa-solid fa-pause fa-xl','fa-solid fa-play fa-xl'],
+            videoStatus:0,
+            meetFutureVid:1,
+            headings:['Values','Mission','Vision'],
+            schoolValues:[
+                'At Wisdom Tree School, our core values serve as the foundation for everything we do. We embrace the values of knowledge, integrity, collaboration, holistic development, and inclusivity.These values guide our interactions, decisions, and the overall learning environment, ensuring that we provide a nurturing, respectful, and inclusive space for all students.',
+                'Our mission at Wisdom Tree School is to inspire and empower young minds to become lifelong learners, critical thinkers, and compassionate individuals. We are committed to fostering a love for learning, providing a holistic education that nurtures academic excellence, character development, and personal growth. Through a supportive and inclusive environment.',
+                'Our vision at Wisdom Tree School is to be a leading educational institution that prepares students for success in the 21st century. We envision a school where students are engaged in meaningful and innovative learning experiences, where they develop a deep understanding of diverse perspectives, and where they are motivated to become responsible global citizens.',
+            ],
+            slectedValue:0,
         }
     },
     methods: {
-        
+        toggleVideo(){
+            if (this.videoStatus === 0){
+                document.getElementById('vid').pause()
+                this.videoStatus =1
+            }else{
+                this.videoStatus =0
+                document.getElementById('vid').play()
+            }
+            
+        },
+        meetFutureToggle(){
+            if (this.meetFutureVid == 1) {
+                this.meetFutureVid =0
+                document.getElementById('meetFutureVid').play()
+            }else{
+                this.meetFutureVid =1
+                document.getElementById('meetFutureVid').pause()
+            }
+        },
+        mouseofset(){
+            console.log(window.OffscreenCanvas)
+        },
+    },
+    mounted() {
+        setInterval(() => {
+            if (this.slectedValue < this.schoolValues.length-1) {
+                this.slectedValue+=1
+            }else{
+                this.slectedValue = 0
+            }
+        }, 5000);
+    
     },
 }).mount('#app')
